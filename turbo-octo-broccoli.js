9d3edd5fe743d7bf58377e4d774686f81d1b1996
AceOfSpades99 <48960871+AceOfSpades99@users.noreply.github.com>
2020-02-05 14:38:31 -0500

    Fourth Commit

--- turbo-octo-broccoli.js
+++ turbo-octo-broccoli.js
@@ -1,2 +1,31 @@
+const readline = require('readline');
+
 console.log('TESTING');
 // TODO(1) MAKE A BOT #TestTODO
+
+const reader = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+	prompt: 'TurdBroShell> ',
+});
+
+reader.prompt();
+
+reader.on('line', (line) => {
+	switch (line.trim()) {
+	case 'hello':
+		console.log('Hello!');
+		break;
+	case 'exit':
+		console.log('Goodbye!');
+		process.exit(0);
+		break;
+	default:
+		console.log(`Say what? I might have heard '${line.trim()}'`);
+		break;
+	}
+	reader.prompt();
+}).on('close', () => {
+	console.log('Have a great day!');
+	process.exit(0);
+});
